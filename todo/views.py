98823be3cc469299490495ce198daf0f1cc449c1
Roberamelaek <Robaman2231999@gmail.com>
2023-11-11 18:09:06 -0500

    the file txt

--- todo/views.py
+++ todo/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 def listen_view(request):
     print("Request received!")
+    file_path = '/home/pomolist/file.txt'
+
+    with open(file_path, 'w') as file:
+        file.write("Requecest received")
+
     return HttpResponse("Request received successfully.")
