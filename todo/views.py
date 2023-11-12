c04f31128aabe3b20fca15fa96235df1847e2063
Roberamelaek <Robaman2231999@gmail.com>
2023-11-11 21:09:34 -0500

    this is the todo list partnnn

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,38 @@ from django.http import HttpResponse, JsonResponse
 import subprocess
 import os
 from django.views import View
-import markdown
+
+from django.conf import settings
+
+import markdown2  # Use markdown2 instead of markdown
+
+class TodoListView(View):
+    def get(self, request, *args, **kwargs):
+        todo_files_directory = getattr(settings, 'TODO_FILES_DIRECTORY', '.')
+        todo_files = [os.path.join(todo_files_directory, f) for f in os.listdir(todo_files_directory) if f.endswith('.md')]
+
+        todos_data = []
+        for todo_file in todo_files:
+            with open(todo_file, 'r') as file:
+                content = file.read()
+                title, description = self.extract_title_and_description(content)
+
+                todos_data.append({
+                    'title': title,
+                    'description': description,
+                })
+
+        return JsonResponse({'todos': todos_data})
+
+    def extract_title_and_description(self, content):
+        lines = content.split('\n')
+        if lines:
+            title_line = lines[0].strip('#').strip()
+            title = markdown2.markdown(title_line, extras=["fenced-code-blocks"])
+            description = '\n'.join(lines[1:]).strip()
+            return title, description
+        return '', ''
+
 
 def listen_view(request):
     repo_path = '/home/pomolist/Flexo'  # Specify the correct path to your Git repository
@@ -22,30 +53,3 @@ def listen_view(request):
     
     return HttpResponse(response_message)
 
-from django.conf import settings
-
-class TodoListView(View):
-    def get(self, request, *args, **kwargs):
-        todo_files_directory = getattr(settings, 'TODO_FILES_DIRECTORY', '.')
-        todo_files = [os.path.join(todo_files_directory, f) for f in os.listdir(todo_files_directory) if f.endswith('.md')]
-        todos_data = []
-        for todo_file in todo_files:
-            with open(todo_file, 'r') as file:
-                content = file.read()
-                title, description = self.extract_title_and_description(content)
-
-                todos_data.append({
-                    'title': title,
-                    'description': description,
-                })
-
-        return JsonResponse({'todos': todos_data})
-
-    def extract_title_and_description(self, content):
-        lines = content.split('\n')
-        if lines:
-            title_line = lines[0].strip('#').strip()
-            title = markdown.markdown(title_line)
-            description = '\n'.join(lines[1:]).strip()
-            return title, description
-        return '', ''
