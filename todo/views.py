be9c7df36d9b7a01b06d6ba236cc741f6edd3622
Roberamelaek <Robaman2231999@gmail.com>
2023-11-11 19:21:29 -0500

    viws/py add

--- todo/views.py
+++ todo/views.py
@@ -1,18 +1,21 @@
-# myapp/views.py
+# todo/views.py
 from django.http import HttpResponse
 import subprocess
 import os
 
-
 def listen_view(request):
-    repo_path = '/home/pomolist/file.txt'
-
-    os.chdir(repo_path)
+    repo_path = '/home/pomolist/Flexo'  # Specify the correct path to your Git repository
 
     try:
-        subprocess(['git','pull'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # Change to the repository directory
+        os.chdir(repo_path)
+        
+        # Run the 'git pull' command
+        subprocess.run(['git', 'pull'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         response_message = "Git pull successful."
     except subprocess.CalledProcessError as e:
         response_message = f"Error during git pull: {e.stderr.decode('utf-8')}"
-
+    except Exception as ex:
+        response_message = f"Unexpected error: {str(ex)}"
+    
     return HttpResponse(response_message)
